3038aa89bd4780c231d37e8c57f71cea2c19f63d
li09.fan <li09.fan@vipshop.com>
2021-10-25 21:28:55 +0800

    add mybatis-learning module

---
+++ mybatis-learning/src/test/java/com/vip/mybatis/mybatislearning/MybatisLearningApplicationTests.java
@@ -0,0 +1,18 @@
+package com.vip.mybatis.mybatislearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    public void test01(){
+
+    }
+
+}
---
+++ test-everything/src/test/java/com/vip/string/Test01.java
@@ -0,0 +1,15 @@
+package com.vip.string;
+
+import org.junit.Test;
+
+/**
+ * @author Ryland
+ */
+public class Test01 {
+
+    @Test
+    public void test01(){
+
+    }
+
+}
